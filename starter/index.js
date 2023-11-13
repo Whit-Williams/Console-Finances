526d10b6e09dee66ad4173d5905fd517c7083585
Whit Will <147825238+Whit-Williams@users.noreply.github.com>
2023-11-13 13:02:28 +0000

    Draft of coding for challenge

---
+++ starter/index.js
@@ -0,0 +1,153 @@
+var finances = [
+  ["Jan-2010", 867884],
+  ["Feb-2010", 984655],
+  ["Mar-2010", 322013],
+  ["Apr-2010", -69417],
+  ["May-2010", 310503],
+  ["Jun-2010", 522857],
+  ["Jul-2010", 1033096],
+  ["Aug-2010", 604885],
+  ["Sep-2010", -216386],
+  ["Oct-2010", 477532],
+  ["Nov-2010", 893810],
+  ["Dec-2010", -80353],
+  ["Jan-2011", 779806],
+  ["Feb-2011", -335203],
+  ["Mar-2011", 697845],
+  ["Apr-2011", 793163],
+  ["May-2011", 485070],
+  ["Jun-2011", 584122],
+  ["Jul-2011", 62729],
+  ["Aug-2011", 668179],
+  ["Sep-2011", 899906],
+  ["Oct-2011", 834719],
+  ["Nov-2011", 132003],
+  ["Dec-2011", 309978],
+  ["Jan-2012", -755566],
+  ["Feb-2012", 1170593],
+  ["Mar-2012", 252788],
+  ["Apr-2012", 1151518],
+  ["May-2012", 817256],
+  ["Jun-2012", 570757],
+  ["Jul-2012", 506702],
+  ["Aug-2012", -1022534],
+  ["Sep-2012", 475062],
+  ["Oct-2012", 779976],
+  ["Nov-2012", 144175],
+  ["Dec-2012", 542494],
+  ["Jan-2013", 359333],
+  ["Feb-2013", 321469],
+  ["Mar-2013", 67780],
+  ["Apr-2013", 471435],
+  ["May-2013", 565603],
+  ["Jun-2013", 872480],
+  ["Jul-2013", 789480],
+  ["Aug-2013", 999942],
+  ["Sep-2013", -1196225],
+  ["Oct-2013", 268997],
+  ["Nov-2013", -687986],
+  ["Dec-2013", 1150461],
+  ["Jan-2014", 682458],
+  ["Feb-2014", 617856],
+  ["Mar-2014", 824098],
+  ["Apr-2014", 581943],
+  ["May-2014", 132864],
+  ["Jun-2014", 448062],
+  ["Jul-2014", 689161],
+  ["Aug-2014", 800701],
+  ["Sep-2014", 1166643],
+  ["Oct-2014", 947333],
+  ["Nov-2014", 578668],
+  ["Dec-2014", 988505],
+  ["Jan-2015", 1139715],
+  ["Feb-2015", 1029471],
+  ["Mar-2015", 687533],
+  ["Apr-2015", -524626],
+  ["May-2015", 158620],
+  ["Jun-2015", 87795],
+  ["Jul-2015", 423389],
+  ["Aug-2015", 840723],
+  ["Sep-2015", 568529],
+  ["Oct-2015", 332067],
+  ["Nov-2015", 989499],
+  ["Dec-2015", 778237],
+  ["Jan-2016", 650000],
+  ["Feb-2016", -1100387],
+  ["Mar-2016", -174946],
+  ["Apr-2016", 757143],
+  ["May-2016", 445709],
+  ["Jun-2016", 712961],
+  ["Jul-2016", -1163797],
+  ["Aug-2016", 569899],
+  ["Sep-2016", 768450],
+  ["Oct-2016", 102685],
+  ["Nov-2016", 795914],
+  ["Dec-2016", 60988],
+  ["Jan-2017", 138230],
+  ["Feb-2017", 671099],
+];
+
+var totalSum = 0;
+
+var totalMonths = finances.length;
+console.log("Total Months: " + totalMonths);
+
+for (var i = 0; i < finances.length; i++) {
+  var netTotal = (totalSum += finances[i][1]);
+}
+console.log("Total: $" + netTotal);
+
+var averageChange = 0;
+var totalAverage = 0;
+var greatestIncrease = ["", 0];
+var greatestDecrease = ["", 0];
+
+for (var i = 0; i < finances.length; i++) {
+  var currentMonth = finances[i][1];
+
+  if (i > 0) {
+    var previousMonth = finances[i - 1][1];
+    averageChange = currentMonth - previousMonth;
+  }
+  console.log(averageChange);
+
+  if (averageChange > greatestIncrease[1]) {
+    greatestIncrease = [finances[i][0], averageChange];
+  }
+
+  if (averageChange < greatestDecrease[1]) {
+    greatestDecrease = [finances[i][0], averageChange];
+  }
+}
+console.log(averageChange);
+console.log(
+  "Average Change: " + (averageChange / (totalMonths - 1)).toFixed(2)
+);
+console.log(
+  "Greatest Increase in Profits/Losses: " +
+    greatestIncrease[0] +
+    " ($" +
+    greatestIncrease[1] + ")"
+);
+console.log(
+  "Greatest Decrease in Profits/Losses: " +
+    greatestDecrease[0] +
+    " ($" +
+    greatestDecrease[1] + ")"
+);
+
+
+//to lessen decimal places you use .toFixed(insert the decimal place you want it to stop after i.e. 2 for after the second decimal number)
+// Create variables to hold: total months, total profit, net total of profit loss, average change, greatest increase and greatest decrease
+// Create for loop to look through the array
+// In each iteration you want to increment the months by one using i++ , then + or - the value depending on if it is positive or negative
+// How to specify which value you want to look at within the array, would be variable names [0][1]
+// how to create an array out of logged iterations = var name.push(the var you want it to be stored)
+//you can create more than one loop
+
+//1. Create console.log that will count the total number of months
+//2. Create console.log that will calculate the total amount of profit/losses
+//3. Calculate the changes in profit/losses between each month
+//4. Use the information from the monthly comparisons to calculate the average
+//5. Use the information from the monthly comparisons to identify the greatest increase
+//6. Track some things whilst we iterate - so we need a loop
